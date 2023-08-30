2123aeb1b23bf5e4a4765885082fec9203e81b23
Parul Bhardwaj <111934039+ParulK-bhardwaj@users.noreply.github.com>
2023-08-30 14:00:48 -0400

    challenge 3

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const dys = ['Sun', 'Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat']
+
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+const mons = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
 class D {
 	constructor(...args) {
 		this._date = new Date(...args);
@@ -12,31 +18,19 @@ class D {
 	}
 
     get month() {
-        const months = ['January', 'February', 'March', 'April', 'May', 'June',
-        'July', 'August', 'September', 'October', 'November', 'December'
-        ]
-
         return months[this._date.getMonth()] // returns month
     }
 
     get mon() {
-        const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-        'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
-        ]
-
-        return months[this._date.getMonth()] // returns short month
+        return mons[this._date.getMonth()] // returns short month
     }
 
     get day() {
-        const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-
         return days[this._date.getDay()] // returns day
     }
 
     get dy() {
-        const days = ['Sun', 'Mon', 'Tues', 'Wed', 'Thur', 'Fri', 'Sat']
-
-        return days[this._date.getDay()] // returns short day
+        return dys[this._date.getDay()] // returns short day
     }
 
     get date() {
@@ -56,6 +50,40 @@ class D {
     }
 
     // Challenge 3
+
+    format( mask = 'Y M D') {
+        const dateStr = mask.split('').map(char => {
+            switch (char) {
+                case 'Y':
+                    return this.year
+                case 'y':
+                    return this.yr
+                case 'M':
+                    return this.month
+                case 'm':
+                    return this.mon
+                case 'D':
+                    return this.date.toString().padStart(2, '0')
+                case 'd':
+                    return this.date
+                case 'H':
+                    return this.hours.toString().padStart(2, '0')
+                case 'h':
+                    return this.hours
+                case 'I':
+                    return this.mins.toString().padStart(2, '0')
+                case 'i':
+                    return this.mins
+                case 'S':
+                    return this.secs.toString().padStart(2, '0')
+                case 's':
+                    return this.secs
+                default:
+                    return char
+            }       
+        } ).join('')
+        return dateStr
+    }
 }
 // The date Object can be initialized in different ways:
 
@@ -82,3 +110,15 @@ console.log( d.hours )
 console.log( d.mins )  
 console.log( d.secs )
 
+// Challenge 3
+
+const e = new D(2023, 0, 2, 3, 4, 5)
+console.log(e.format())              // 2017 January 02
+console.log(e.format('y/m/D'))       // 17/Jan/2
+console.log(e.format('H:I:S'))       // 03:04:05
+console.log(e.format('h:i:s'))       // 3:4:5
+console.log(e.format('Y-M-D h:I:S'))
+// const yesterday = new D(2023, 7, 29, 3, 4, 5)
+// const today = new D(2023, 7, 30, 3, 4, 5)
+// console.log(yesterday._date - today._date)
+// console.log(yesterday._date.getSeconds() - today._date.getSeconds())
