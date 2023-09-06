0caef47d03b20f527e1d8f4f09801a4022551a9f
Parul Bhardwaj <111934039+ParulK-bhardwaj@users.noreply.github.com>
2023-09-05 23:57:20 -0400

    tests completed

--- index.test.js
+++ index.test.js
@@ -68,4 +68,28 @@ describe('D class', () => {
             expect(dateInstance.format('Y-M-D h:I:S')).toBe('2023-March-18 9:01:05')
         })
     })
+
+    // when() method
+
+    describe('when', () => {
+        const futureDate = new D(2025, 8, 5, 3, 4, 5)
+        const pastDate = new D(2022, 1, 5, 3, 4, 5)
+
+        test('should handle future dates correctly',() =>{
+            expect(futureDate.when()).toBe('2 years from now')
+        })
+
+        test('should handle past dates correctly',() =>{
+            expect(pastDate.when()).toBe('1 year ago')
+        })
+
+        test('should handle past dates correctly',() =>{
+            expect(pastDate.when()).toBe('1 year ago')
+        })
+
+        test('Today', () => {
+            const currentDate = new D()
+            expect(currentDate.when()).toBe('today');
+        })
+    })
 })
